9ef738ce0977c0cded98b16615ab971e57ea8d57
Ahmed Maher <ahmedxsalman@outlook.sa>
2025-04-13 21:06:23 +0200

    Add project files.

---
+++ Hospital/Controllers/AppointmentController.cs
@@ -0,0 +1,49 @@
+﻿using Hospital.Data;
+using Microsoft.AspNetCore.Mvc;
+using Hospital.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext = new ApplicationDbContext();
+        public IActionResult Index()
+        {
+            var appointments = _dbContext.Appiontments.Include(e=> e.Doctor);
+            return View(appointments.ToList());
+        }
+
+        
+        public IActionResult Book( int id)
+        {
+            var doc = _dbContext.Doctors.FirstOrDefault(d => d.Id == id);
+            return View(doc);
+        }
+        [HttpPost]
+        public IActionResult Book( Appiontment appoint , int DoctorId)
+        {
+
+            var doc = _dbContext.Doctors.FirstOrDefault(d => d.Id == DoctorId);
+            if (appoint.Date < DateOnly.FromDateTime(DateTime.Now))
+            {
+                return View( doc);
+            }
+            
+
+            
+            _dbContext.Add(new Appiontment
+            {
+                PatientName = appoint.PatientName,
+                
+                Date = appoint.Date,
+                Time = appoint.Time,
+                DoctorId = doc.Id
+            });
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+
+
+}
---
+++ Hospital/Controllers/DoctorController.cs
@@ -0,0 +1,41 @@
+﻿using Hospital.Data;
+using Hospital.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Hospital.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext = new ();
+
+      
+
+        public IActionResult Index(string Specialization, string? query)
+        {
+            IQueryable<Appiontment> appiontments = _dbContext.Appiontments.Include(a => a.Doctor);
+
+            var doctors = _dbContext.Doctors.AsQueryable();
+            ViewData["doctors"] = doctors.ToList();
+
+            if (!string.IsNullOrEmpty(Specialization))
+            {
+                doctors = doctors.Where(d => d.Specialization == Specialization);
+                ViewBag.Specialization = Specialization;
+            }
+
+            if (!string.IsNullOrEmpty(query))
+            {
+                doctors = doctors.Where(d => d.Name.Contains(query));
+                ViewBag.query = query;
+            }
+
+            return View(doctors.ToList());
+        }
+
+        public IActionResult NotFoundPage()
+        {
+            return View();
+        }
+    }
+}
